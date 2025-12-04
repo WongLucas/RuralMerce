de172ed513bdd14fede47d43f04449dd9eb1bbec
Lucas <lucaswongalmeida@gmail.com>
2025-12-04 19:31:47 -0300

    Antigo itens removidos

--- start/routes.ts
+++ start/routes.ts
@@ -1,7 +1,5 @@
 import router from '@adonisjs/core/services/router'
 
-import ItemsController from '#controllers/items_controller'
-import CategoriesController from '#controllers/categories_controller'
 import UsersController from '#controllers/users_controller'
 import SessionController from '#controllers/session_controller'
 import ProfilesController from '#controllers/profiles_controller'
@@ -12,13 +10,6 @@ import CartsController from '#controllers/carts_controller'
 
 //Rotas que devem estar logadas
 router.group(() => {
-  //Rotas para os itens
-  router.resource('/item', ItemsController)
-
-  //Rotas para criação de categorias
-  router.get('/category',[CategoriesController, 'create']).as('category.create')
-  router.post('/category',[CategoriesController, 'store']).as('category.store')
-
   //Rota para poder deslogar
   router.post('/logout', [SessionController, 'destroy']).as('logout')
 
